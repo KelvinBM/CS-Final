4f2f7dbb378e4e0a1495d741a3abbef69a9d8749
miken <coulibalyhamet862@gmail.com>
2025-12-21 04:00:01 -0500

    Gameplay loop

--- Fighting.cpp
+++ Fighting.cpp
@@ -12,8 +12,37 @@ using namespace std;
 int main() {
   //  Intro Test;
  //   Test.GameIntroduction();
-    Game play = Game(7);
-    play.PlayGame();
+    bool playAgain = true;
+    cout << "Hello Player this game is a turned based fighting game where you choose a hero and fight against villains." << endl;
+    cout << "You can also play the story mode if you want more than just the fighting aspect." << endl;
+    cout << "Which would you like to do \n 1. Fighting Mode \n 2. Story Mode" << endl;
+    int modeChoice;
+    cin >> modeChoice;
+    if (modeChoice == 1) {
+        cout << "You have chosen Fighting Mode" << endl;
+        Game fightingGame = Game();
+        while (playAgain) {
+            fightingGame.PlayGame();
+            cout << "Would you like to play again? (1 for yes, 2 for no)" << endl;
+            int Replay;
+            cin >> Replay;
+            if (Replay == 1) {
+                playAgain = true;
+            }
+            else {
+                playAgain = false;
+            }
+        }
+        return 0;
+    }
+    else if (modeChoice == 2) {
+        cout << "You have chosen the Story Mode" << endl;
+        cout << "Before you start your adventure what would you like to be called?" << endl;
+        string playerName;
+        getline(cin, playerName);
+        cout << "Welcome " << playerName << " what class would you like to be?" << endl;
+    }
+
 
 
     return 0;
