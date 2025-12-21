d1f3e1da063a516c933e1248cd01eeff7737ae91
miken <coulibalyhamet862@gmail.com>
2025-12-21 15:41:29 -0500

    Just gradually updating

--- Intro.h
+++ Intro.h
@@ -3,12 +3,14 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include "WeaponChoice.h"
 
-    void Prologue(string name) {
+    int Prologue(string name) {
         cout << "You wake up in a dungeon with no memory of how you got there." << endl;
         cout << "And your head is bleeding..." << endl;
         cout << "When you finally come to your senses you see a giant creature with a cloak standing in front of you." << endl
         << endl << endl << endl;
+        this_thread::sleep_for(chrono::seconds(5));
 
         cout << "                                         .""--..__" << endl;
         cout << "                     _                     []       ``-.._" << endl;
@@ -49,21 +51,32 @@
         this_thread::sleep_for(chrono::seconds(2));
         cout << "You look at it and where its face should be, there is only darkness as well as a eerie feeling as you stare into it." << endl;
         cout << "Its so large that you have to look straight up to even see its head." << endl;
-        this_thread::sleep_for(chrono::seconds(1));
+        this_thread::sleep_for(chrono::seconds(3));
         cout << "Finally it speaks in a bellowing voice that seems to echo through the entire dungeon" << endl;
         cout << "Its voice sends chills down your spine..." << endl << endl;
-        this_thread::sleep_for(chrono::seconds(1));
-        cout << "Shadow Being: Welcome " << name << "I was wondering when you would wake up." << endl;
-        this_thread::sleep_for(chrono::seconds(1));
+        this_thread::sleep_for(chrono::seconds(2));
+        cout << "Shadow Being: Welcome " << name << " I was wondering when you would wake up." << endl;
+        this_thread::sleep_for(chrono::seconds(2));
         cout << name << ": Who... who are you? Where am I!" << endl;
-        this_thread::sleep_for(chrono::seconds(1));
+        this_thread::sleep_for(chrono::seconds(2));
         cout << "Shadow Being: Silence. You have been chosen for a great purpose." << endl;
         cout << name << ": Chosen? For what?" << endl;
-        this_thread::sleep_for(chrono::seconds(1));
-        cout << "You are the decendant of the ancient hero who once saved this world from destruction. Kael Thunderborn" << endl;
+        this_thread::sleep_for(chrono::seconds(2));
+        cout << "You are the descendant of the ancient hero who once saved this world from destruction. Kael Thunderborn" << endl;
         cout << "And now it is your turn to save the world from the impending doom that is awaiting.\n But first you must prove yourself worthy." << endl;
-        cout << "*The creature hits the ground with its staff, you fall over from the shaking of the ground but 2 pedestals rise from the ground \n So bright that you would think they are the sun itself*" << endl;
+        this_thread::sleep_for(chrono::seconds(2));
+        cout << "*The creature hits the ground with its staff, you fall over from the shaking of the ground but 2 pedestals rise from the ground, So bright that you would think they are the sun itself*" << endl;
+        cout << "Shadow Being: On each pedestal lies a weapon from your ancestor. Choose wisely " << name << "." << endl << endl << endl;
+        int weaponChosen = WeaponChoice();
+        if (weaponChosen == 1) {
+            cout << "Shadow Being: Ah, the Thunder Fang. A fine choice indeed." << endl;
+        }
+        else {
+            cout << "Shadow Being: The Sky Piercers, swift and deadly. An excellent choice." << endl;
+        }
+        this_thread::sleep_for(chrono::seconds(2));
 
+        return weaponChosen;
     }
 
 
---
+++ WeaponChoice.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <chrono>
+#include <thread>
+
+int WeaponChoice() {
+    cout << "PEDESTAL 1: A Shining Sword with sparks of lightning around the blade." << endl;
+    cout << "Thunder Fang: A sword that uses the power of the storm to strike enemies with lightning." << endl << endl;
+    this_thread::sleep_for(chrono::seconds(2));
+    cout << "PEDESTAL 2: Twin daggers that sound as if your in the heart of a storm" << endl;
+    cout << "Sky Piercers: Two daggers that allow the user to strike with the speed of lightning." << endl << endl;
+    this_thread::sleep_for(chrono::seconds(2));
+    cout << "Which weapon would you like to choose? \n 1) Thunder Fang \n 2) Sky Piercers?" << endl;
+    int weapon = 0;
+    cin >> weapon;
+    if (weapon == 1) {
+        cout << "You have chosen Thunder Fang..." << endl;
+        cout << "You grab the sword and feel a powerful surge of electricity flow through your body." << endl;
+        cout << "You feel as if you can command the power of the storm itself..." << endl;
+    }
+    else if (weapon == 2) {
+        cout << "You have chosen Sky Piercers..." << endl;
+        cout << "As you pick up the daggers, you feel a surge of energy coursing through your veins." << endl;
+        cout << "You feel as if you can move with the speed of lightning itself..." << endl;
+    }
+    return weapon;
+}
