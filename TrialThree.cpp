c3e88bbc3b261836a26a7041be4f750a21a7ee1a
miken <coulibalyhamet862@gmail.com>
2025-12-21 19:36:01 -0500

    Trial 2/3

--- TrialThree.cpp
+++ TrialThree.cpp
@@ -1,10 +1,10 @@
 #include "TrialThree.h"
 
 bool TrialThree(Game game, Player player) {
-    Attacks enemyAttacks = player.GetCharacterAttacks();
+    Attacks enemyAttacks = player.GetCharacterAttacks(); // Copy player's attacks and give enemy shadow name
     string enemyName = "Shadow " + player.GetName();
 
     Player enemy(enemyName, player.GetMaxHealth(), player.GetAttackDamage(), player.GetCooldown(), player.GetPlayerChoice(), enemyAttacks);
 
-    return game.StoryModeFight(player, enemy);
+    return game.StoryModeFight(player, enemy); // return true or false for the worthiness meter thing
 }
